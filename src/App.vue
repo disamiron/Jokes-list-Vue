48918ff5632f8d62aed4fdcd61011eac731f868a
Mironov Denis <mirronovdenis@gmail.com>
2021-08-22 15:56:12 +0300

    +api jokes function
    10 jokes download + display

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,35 @@
 <template>
   <h1>10 jokes for fun</h1>
+
+    <ol>
+    <li v-for="{ id, joke } in jokes" v-bind:key="id">
+      {{ joke }}
+    </li>
+  </ol>
 </template>
+
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const jokes = ref(null);
+
+    async function updateJokes() {
+      jokes.value = null;
+
+      const res = await fetch(
+        `https://v2.jokeapi.dev/joke/Any?type=single&amount=10`
+      );
+
+      const data = await res.json();
+      jokes.value = data.jokes;
+    }
+
+    updateJokes();
+
+    return { jokes };
+  },
+};
+</script>
